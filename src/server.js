703796573b31f365cb76339fdbe658580346b432
Vitor Giovane <vitorgpalves@gmail.com>
2019-04-10 13:39:52 -0300

    feature(backend): configura a conexão com o BD
    Adiciona a conexão com um banco de dados MongoDB criado na plataforma
    Atlas
    
    ==
    https://www.mongodb.com/cloud/atlas

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express();
+
+mongoose.connect('mongodb+srv://vitor:nlCt4W8@cluster0-kykki.mongodb.net/omnistack?retryWrites=true',
+{
+  useNewUrlParser: true
+})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
