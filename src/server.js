000a0075258a459383a902ed01f53d1f0756dcf3
Vitor Giovane <vitorgpalves@gmail.com>
2019-04-10 13:33:56 -0300

    feature(backend): isola as rotas da aplicação em um arquivo
    Cria arquivo routes.js para centralizar todas as rotas da aplicação

--- src/server.js
+++ src/server.js
@@ -5,8 +5,6 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/test', (req, res) => {
-  res.send('Hello, Vitor!')
-})
+app.use(require('./routes'))
 
 app.listen(3333)
